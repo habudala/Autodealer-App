218cbc79cc12c71f351a0444dbdb2943e9411a5d
habudala <habudala@live.com>
2016-06-06 06:36:22 -0400

    Some Updates
    I made some fundamental styling and layout decisions, added ng-click
    directive to buttons, and hooked them each to a separate function.

--- scripts/script.js
+++ scripts/script.js
@@ -16,23 +16,9 @@ var app = angular.module("myModule",[]);
 
 app.controller("myController", function($scope,$http){
 
-https://api.themoviedb.org/3/movie/now_playing
+//https://api.themoviedb.org/3/movie/now_playing
 
-	$http({
-        method : "GET",
-        url : "https://api.themoviedb.org/3/movie/now_playing?api_key=5ce8b69de33584132f1db727eb5cb513"
-    })
-    .then(function mySucces(response) {
-        // $scope.myWelcome = response.data;
-        console.log(response);
-    },
-     function myError(response) {
-        $scope.myWelcome = response.statusText;
-    });
-
-
-
-    $http({
+$http({
         method : "GET",
         url : "https://api.themoviedb.org/3/search/multi?api_key=5ce8b69de33584132f1db727eb5cb513&query=batman"
     })
@@ -44,6 +30,101 @@ https://api.themoviedb.org/3/movie/now_playing
         $scope.myWelcome = response2.statusText;
     });
 
+
+
+$scope.nowPlaying = function () {
+
+        $http({
+        method : "GET",
+        url : "https://api.themoviedb.org/3/movie/now_playing?api_key=5ce8b69de33584132f1db727eb5cb513"
+    })
+    .then(function mySucces(response) {
+        // $scope.myWelcome = response.data;
+        // console.log(response.data.results[0]);
+        $scope.myArray = response.data.results;
+    },
+     function myError(response) {
+        $scope.myWelcome = response.statusText;
+    });
+
+
+}
+
+
+
+    $scope.popular = function () {
+
+        $http({
+        method : "GET",
+        url : "https://api.themoviedb.org/3/movie/popular?api_key=5ce8b69de33584132f1db727eb5cb513"
+    })
+    .then(function mySucces(response3) {
+        // $scope.myWelcome = response.data;
+        console.log(response3);
+        $scope.myArray = response3.data.results;
+    },
+     function myError(response3) {
+        $scope.myWelcome = response3.statusText;
+    });
+
+}
+
+    $scope.upcoming = function () {
+
+        $http({
+        method : "GET",
+        url : "https://api.themoviedb.org/3/movie/upcoming?api_key=5ce8b69de33584132f1db727eb5cb513"
+    })
+    .then(function mySucces(response4) {
+        // $scope.myWelcome = response.data;
+        console.log(response4);
+        $scope.myArray = response4.data.results;
+    },
+     function myError(response4) {
+        $scope.myWelcome = response4.statusText;
+    });
+   
+}
+
+    $scope.topRated = function () {
+
+        $http({
+        method : "GET",
+        url : "https://api.themoviedb.org/3/movie/upcoming?api_key=5ce8b69de33584132f1db727eb5cb513"
+    })
+    .then(function mySucces(response5) {
+        // $scope.myWelcome = response.data;
+        console.log(response5);
+        $scope.myArray = response5.data.results;
+    },
+     function myError(response5) {
+        $scope.myWelcome = response5.statusText;
+    });
+   
+}
+
+    $scope.genres = function () {
+
+        $http({
+        method : "GET",
+        url : "https://api.themoviedb.org/3/movie/upcoming?api_key=5ce8b69de33584132f1db727eb5cb513"
+    })
+    .then(function mySucces(response6) {
+        // $scope.myWelcome = response.data;
+        console.log(response6);
+        $scope.myArray = response6.data.results;
+    },
+     function myError(response6) {
+        $scope.myWelcome = response6.statusText;
+    });
+   
+}
+
+    $scope.extras = function () {
+
+    
+   
+}
 
 	
 
